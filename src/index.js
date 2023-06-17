43e57e7f3197e0afba4c610da69da16bfe89b1cd
SolarPal <tumaf@mail.ru>
2023-06-17 20:43:44 +0300

    feat: implement the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+	if (n < 0 | n > 9) {
+		let a = [];
+		let r = "";
+		for (let i = Array.from(Math.abs(n).toString()).length - 1; i >= 0; i -= 1) {
+			a.push(Array.from(Math.abs(n).toString())[i]);
+		}
+		for (let i = 0; i < a.length; i += 1) {
+			r += a[i];
+		}
+		return Number(r);
+	}  else {
+		return n;
+	}
 }
